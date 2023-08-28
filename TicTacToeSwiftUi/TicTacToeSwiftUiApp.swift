c3a7b4e3d5a84e0715caab7283a2485c1cf3b56d
suayip-isik-dijital-sahne <136647667+suayip-isik-dijital-sahne@users.noreply.github.com>
2023-08-28 20:54:05 +0300

    Initial Commit

---
+++ TicTacToeSwiftUi/TicTacToeSwiftUiApp.swift
@@ -0,0 +1,17 @@
+//
+//  TicTacToeSwiftUiApp.swift
+//  TicTacToeSwiftUi
+//
+//  Created by Şuayip Işık on 28.08.2023.
+//
+
+import SwiftUI
+
+@main
+struct TicTacToeSwiftUiApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
